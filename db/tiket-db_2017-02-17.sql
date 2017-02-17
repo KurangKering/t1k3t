83e7d3793f9df263facfe7cd53a278fcded3b991
KurangKering <cybercature@gmail.com>
2017-02-17 23:34:09 +0700

    update database

--- db/tiket-db_2017-02-17.sql
+++ db/tiket-db_2017-02-17.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 17, 2017 at 10:07 AM
+-- Generation Time: Feb 17, 2017 at 04:32 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.1
 
@@ -31,7 +31,7 @@ USE `tiket`;
 CREATE TABLE `maskapai` (
   `id_maskapai` int(11) NOT NULL,
   `nama` varchar(30) NOT NULL,
-  `fee` decimal(10,0) NOT NULL,
+  `fee` int(11) NOT NULL,
   `persen` decimal(5,4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -40,11 +40,11 @@ CREATE TABLE `maskapai` (
 --
 
 INSERT INTO `maskapai` (`id_maskapai`, `nama`, `fee`, `persen`) VALUES
-(1, 'AIR ASIA', '10000', '5.0000'),
-(2, 'SRIWIJAYA', '10000', '5.0000'),
-(3, 'CITILINK', '10000', '5.0000'),
-(4, 'LION', '10000', '5.0000'),
-(5, 'BATIK', '10000', '5.0000');
+(1, 'AIR ASIA', 10000, '5.0000'),
+(2, 'SRIWIJAYA', 10000, '5.0000'),
+(3, 'CITILINK', 10000, '5.0000'),
+(4, 'LION', 10000, '5.0000'),
+(5, 'BATIK', 10000, '5.0000');
 
 -- --------------------------------------------------------
 
@@ -62,8 +62,15 @@ CREATE TABLE `penjualan` (
   `persen` decimal(5,4) NOT NULL,
   `invoice` int(11) NOT NULL,
   `q` tinyint(4) NOT NULL,
-  `fee` int(11) NOT NULL
+  `adm_fee` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_penjualan`, `id_tc`, `id_maskapai`, `tanggal`, `booking_code`, `hpp`, `persen`, `invoice`, `q`, `adm_fee`) VALUES
+(1, 1, 1, '2017-02-01', 'J9QUNW', 634100, '0.0500', 754100, 3, 10000);
 
 -- --------------------------------------------------------
 
@@ -101,6 +108,43 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`username`, `password`) VALUES
 ('admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `view_penjualan`
+-- (See below for the actual view)
+--
+CREATE TABLE `view_penjualan` (
+`id_penjualan` int(11)
+,`id_tc` int(11)
+,`id_maskapai` int(11)
+,`tanggal` date
+,`booking_code` varchar(10)
+,`hpp` int(11)
+,`persen` decimal(5,4)
+,`invoice` int(11)
+,`q` tinyint(4)
+,`adm_fee` int(11)
+,`nama_tc` varchar(50)
+,`nama_maskapai` varchar(30)
+,`NTA` bigint(17) unsigned
+,`harga_jual` bigint(18) unsigned
+,`up_salling` bigint(19) unsigned
+,`profit_1` bigint(20) unsigned
+,`FEE` bigint(14)
+,`profit_2` bigint(21) unsigned
+,`jumlah` bigint(15)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `view_penjualan`
+--
+DROP TABLE IF EXISTS `view_penjualan`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`127.0.0.1` SQL SECURITY DEFINER VIEW `view_penjualan`  AS  select `penjualan`.`id_penjualan` AS `id_penjualan`,`penjualan`.`id_tc` AS `id_tc`,`penjualan`.`id_maskapai` AS `id_maskapai`,`penjualan`.`tanggal` AS `tanggal`,`penjualan`.`booking_code` AS `booking_code`,`penjualan`.`hpp` AS `hpp`,`penjualan`.`persen` AS `persen`,`penjualan`.`invoice` AS `invoice`,`penjualan`.`q` AS `q`,`penjualan`.`adm_fee` AS `adm_fee`,`tc`.`nama_tc` AS `nama_tc`,`maskapai`.`nama` AS `nama_maskapai`,cast((`penjualan`.`hpp` * `penjualan`.`persen`) as unsigned) AS `NTA`,cast((`penjualan`.`hpp` + (`penjualan`.`hpp` * `penjualan`.`persen`)) as unsigned) AS `harga_jual`,cast((`penjualan`.`invoice` - (`penjualan`.`hpp` + (`penjualan`.`hpp` * `penjualan`.`persen`))) as unsigned) AS `up_salling`,cast(((`penjualan`.`hpp` * `penjualan`.`persen`) + (`penjualan`.`invoice` - (`penjualan`.`hpp` + (`penjualan`.`hpp` * `penjualan`.`persen`)))) as unsigned) AS `profit_1`,(`penjualan`.`q` * `penjualan`.`adm_fee`) AS `FEE`,cast((((`penjualan`.`hpp` * `penjualan`.`persen`) + (`penjualan`.`invoice` - (`penjualan`.`hpp` + (`penjualan`.`hpp` * `penjualan`.`persen`)))) - (`penjualan`.`q` * `penjualan`.`adm_fee`)) as unsigned) AS `profit_2`,(`penjualan`.`hpp` + (`penjualan`.`q` * `penjualan`.`adm_fee`)) AS `jumlah` from ((`penjualan` join `maskapai` on((`penjualan`.`id_maskapai` = `maskapai`.`id_maskapai`))) join `tc` on((`penjualan`.`id_tc` = `tc`.`id_tc`))) ;
 
 --
 -- Indexes for dumped tables
@@ -143,7 +187,7 @@ ALTER TABLE `maskapai`
 -- AUTO_INCREMENT for table `penjualan`
 --
 ALTER TABLE `penjualan`
-  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `tc`
 --
